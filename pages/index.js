56f1bceab8ca19c690d3ff2d3d3afa38e6b50460
Valdemar Katayama Kjaer <valdemarkjaer@gmail.com>
2023-12-01 23:00:08 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jaqueline, amo você. Se você me ama, dá uma risadinha
+    return <h1>Jaqueline, amo você. Se você me ama, dá uma risadinha - deploy 2 para testar teste vercel
     </h1>
 }
 
