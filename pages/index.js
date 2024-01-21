58a8c57e603a35362631a7cafc7c9b7cad5be1d6
Valdemar Katayama Kjaer <valdemarkjaer@gmail.com>
2024-01-21 14:02:19 +0000

    jan24

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Deploy final. Apenas para constar, o curso é muito, mas muito bom mesmo.
-      Didática 110%. Parabéns!
+      Didática 110%. Parabéns!!!
     </h1>
   );
 }
