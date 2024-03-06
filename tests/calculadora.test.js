af3b524c9f25fd7cd7c46360a339e1c6fa125ead
Valdemar Katayama Kjaer <valdemarkjaer@gmail.com>
2024-03-06 04:18:47 +0000

    atualizacao do commit anterior. ainda falta fazer o teste de verdade do dia 15 do curso do Felipe Deschamps

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -17,7 +17,7 @@ test("Testando outra condição do sistema", () => {
 
 test("espero que 1 seja 1", () => {    
     expect(1).toBe(1);     // expect é uma função do framework de testes jest. Aqui está: espere que 1 seja 1. Um teste tem dois lados. Esquerda: valor gerado por sistema, dinamico. Direita = expectativa. Hard Code é melhor
-    expect(2).toBe(1);     // aqui quebra
+    // expect(2).toBe(1);     // aqui quebra
     // expect(funcaoSendoTestadaGeradaDinamica).toBe(resultadoFixo);  // chamada de um calculo do lado esquerdo gera tal valor do lado direito
 
 });
